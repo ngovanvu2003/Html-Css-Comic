e8e4d82de3471a7e1767934a222e0504f2e9f4f3
ngovanvu2003 <ngovanvu203@gmail.com>
2024-08-03 17:43:43 +0700

    main

---
+++ assets/js/main.js
@@ -0,0 +1,34 @@
+let themeToggler = document.querySelector("#theme-toggler");
+
+themeToggler.onclick = () => {
+  themeToggler.classList.toggle("fa-sun");
+  if (themeToggler.classList.contains("fa-sun")) {
+    document.body.classList.add("active");
+  } else {
+    document.body.classList.remove("active");
+  }
+};
+document.addEventListener("DOMContentLoaded", function () {
+  const themeToggler = document.querySelector("#on-search");
+  const content = document.querySelector("#content");
+  const overlay = document.querySelector("#overlay");
+
+  themeToggler.addEventListener("click", function () {
+    content.classList.toggle("show");
+    overlay.classList.toggle("show");
+  });
+
+  // Thêm sự kiện click cho overlay và document để ẩn nội dung khi click ra ngoài
+  document.addEventListener("click", function (event) {
+    if (content.classList.contains("show")) {
+      // Kiểm tra nếu người dùng nhấn ra ngoài nội dung
+      if (
+        !content.contains(event.target) &&
+        !themeToggler.contains(event.target)
+      ) {
+        content.classList.remove("show");
+        overlay.classList.remove("show");
+      }
+    }
+  });
+});
